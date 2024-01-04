04ddb00e4c6809a607f94a21ff27d1d492ab2c08
kadeland <sarahwaseem21@gmail.com>
2024-01-04 11:07:36 +0500

    meme-generator-scrimba

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../App.css"
+
+export default function Header() {
+return (
+    <header className="heading">
+        <img src="../logo192.png" />
+      <h4>Meme Generator</h4>
+      <h5>React Course</h5>
+    </header>
+)
+}
